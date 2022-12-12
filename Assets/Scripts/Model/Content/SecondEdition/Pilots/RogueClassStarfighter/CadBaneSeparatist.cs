3f7156321c516b1bc73e4396684bcc054e99db5e
sampson-matt <sampson.matt@gmail.com>
2022-12-12 13:10:42 -0600

    Add Cad Bane Separatist

--- Assets/Scripts/Model/Content/SecondEdition/Pilots/RogueClassStarfighter/CadBaneSeparatist.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/RogueClassStarfighter/CadBaneSeparatist.cs
@@ -1,6 +1,10 @@
 ﻿using System.Collections.Generic;
 using System;
 using Upgrade;
+using Ship;
+using MainPhases;
+using BoardTools;
+using ActionsList;
 
 namespace Ship
 {
@@ -10,12 +14,12 @@ namespace Ship
         {
             public CadBaneSeparatist() : base()
             {
-                RequiredMods = new List<Type>() { typeof(Mods.ModsList.UnreleasedContentMod) };
-
                 PilotInfo = new PilotCardInfo(
                     "Cad Bane",
                     4,
                     44,
+                    charges: 1,
+                    regensCharges: 1,
                     pilotTitle: "Needs No Introduction",
                     isLimited: true,
                     abilityType: typeof(Abilities.SecondEdition.CadBaneSeparatistAbility),
@@ -35,14 +39,88 @@ namespace Abilities.SecondEdition
 {
     public class CadBaneSeparatistAbility : GenericAbility
     {
+        private GenericShip PreviousCurrentShip { get; set; }
         public override void ActivateAbility()
         {
-
+            GenericShip.OnShipIsDestroyedGlobal += CheckAbility;
         }
 
         public override void DeactivateAbility()
         {
+            GenericShip.OnShipIsDestroyedGlobal -= CheckAbility;
+        }
+        private void CheckAbility(GenericShip ship, bool flag)
+        {
+            if (!(Phases.CurrentPhase is CombatPhase)|| HostShip.State.Charges < 1)
+                return;
 
+            DistanceInfo distanceInfo = new DistanceInfo(HostShip, ship);
+            if (distanceInfo.Range > 3) return;
+
+            RegisterAbilityTrigger(TriggerTypes.OnShipIsDestroyed, PerformAction);
+        }
+
+        private void PerformAction(object sender, System.EventArgs e)
+        {
+            var ship = Selection.ThisShip;
+            Roster.HighlightPlayer(HostShip.Owner.PlayerNo);
+            Selection.ChangeActiveShip(HostShip);
+
+            CameraScript.RestoreCamera();
+
+            HostShip.OnCanPerformActionWhileStressed += TemporaryAllowAnyActionsWhileStressed;
+            HostShip.OnCheckCanPerformActionsWhileStressed += TemporaryAllowActionsWhileStressed;
+            HostShip.OnActionIsPerformed += DisallowActionsWhileStressed;
+            HostShip.OnActionIsSkipped += DisallowActionsWhileStressedAlt;
+
+            List<GenericAction> actions = Selection.ThisShip.GetAvailableActions();
+
+            Messages.ShowInfoToHuman(HostName + ": you may spend 1 charge to perform an action");
+
+            HostShip.BeforeActionIsPerformed += SpendCharge;
+
+            HostShip.AskPerformFreeAction(
+                actions,
+                delegate {
+                    Roster.HighlightPlayer(ship.Owner.PlayerNo);
+                    Selection.ChangeActiveShip(ship);
+                    Triggers.FinishTrigger();
+                },
+                HostShip.PilotInfo.PilotName,
+                "After another ship at range 0-3 is destroyed, you may spend 1 charge to perform an action, even while stressed",
+                HostShip
+            );
+        }
+
+        private void SpendCharge(GenericAction action, ref bool isFreeAction)
+        {
+            HostShip.BeforeActionIsPerformed -= SpendCharge;
+            HostShip.SpendCharge();
+        }
+
+        private void DisallowActionsWhileStressed(GenericAction action)
+        {
+            HostShip.OnCanPerformActionWhileStressed -= TemporaryAllowAnyActionsWhileStressed;
+            HostShip.OnCheckCanPerformActionsWhileStressed -= TemporaryAllowActionsWhileStressed;
+            HostShip.OnActionIsPerformed -= DisallowActionsWhileStressed;
+        }
+
+        private void DisallowActionsWhileStressedAlt(GenericShip ship)
+        {
+            HostShip.OnCanPerformActionWhileStressed -= TemporaryAllowAnyActionsWhileStressed;
+            HostShip.OnCheckCanPerformActionsWhileStressed -= TemporaryAllowActionsWhileStressed;
+            HostShip.OnActionIsPerformed -= DisallowActionsWhileStressed;
+            HostShip.OnActionIsSkipped -= DisallowActionsWhileStressedAlt;
+        }
+
+        private void TemporaryAllowAnyActionsWhileStressed(GenericAction action, ref bool isAllowed)
+        {
+            isAllowed = true;
+        }
+
+        private void TemporaryAllowActionsWhileStressed(ref bool isAllowed)
+        {
+            isAllowed = true;
         }
     }
 }

17d2835f801c89dc12125c714504f7d191cf2699
sampson-matt <sampson.matt@gmail.com>
2022-12-16 13:22:30 -0600

    Add Moff Gideon Pilot

--- Assets/Scripts/Model/Content/SecondEdition/Pilots/TIELnFighter/MoffGideon.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/TIELnFighter/MoffGideon.cs
@@ -8,6 +8,8 @@ using Editions;
 using SubPhases;
 using Abilities.SecondEdition;
 using Upgrade;
+using BoardTools;
+using ActionsList;
 
 namespace Ship
 {
@@ -25,6 +27,7 @@ namespace Ship
                     isLimited: true,
                     abilityType: typeof(MoffGideonAbility),
                     charges: 2,
+                    regensCharges: 1,
                     extraUpgradeIcons: new List<UpgradeType>() { UpgradeType.Talent, UpgradeType.Talent }
                 );
 
@@ -41,12 +44,69 @@ namespace Abilities.SecondEdition
 
         public override void ActivateAbility()
         {
-
+            GenericShip.OnAttackStartAsAttackerGlobal += CheckAbility;
         }
 
         public override void DeactivateAbility()
         {
+            GenericShip.OnAttackStartAsAttackerGlobal -= CheckAbility;
+        }
 
+        private void CheckAbility()
+        {
+            bool IsDifferentPlayer = (HostShip.Owner.PlayerNo != Combat.Defender.Owner.PlayerNo);
+            DistanceInfo distanceInfo = new DistanceInfo(HostShip, Combat.Defender);
+
+            if (IsDifferentPlayer 
+                &&  distanceInfo.Range <= 3 
+                && distanceInfo.Range >= 1
+                && HostShip.State.Charges > 0)
+            {
+                RegisterAbilityTrigger(TriggerTypes.OnAttackStart, AskChooseFriendlyShip);
+            }
+        }
+
+        private void AskChooseFriendlyShip(object sender, EventArgs e)
+        {
+            SelectTargetForAbility(
+                PayForAbility,
+                FilterTargets,
+                GetFriendlyTargetPriority,
+                HostShip.Owner.PlayerNo,
+                name: HostShip.PilotInfo.PilotName,
+                description: "You may spend a charge token and choose a friendly ship at range 0-1 of the defender to gain a strain token. If you do, defense dice cannot be modified. ",
+                imageSource: HostShip
+            );
+        }
+
+        private void PayForAbility()
+        {
+            HostShip.SpendCharge();
+            TargetShip.Tokens.AssignToken(typeof(StrainToken), SelectShipSubPhase.FinishSelection);
+            Combat.Defender.OnTryAddAvailableDiceModification += PreventDiceModification;
+
+        }
+
+        private void PreventDiceModification(GenericShip ship, GenericAction action, ref bool canBeUsed)
+        {
+            if (Combat.AttackStep == CombatStep.Defence && Combat.Defender.ShipId == ship.ShipId)
+            {
+                ship.OnTryAddAvailableDiceModification -= PreventDiceModification;
+                Messages.ShowInfo("Moff Gideon: The defender cannot use " + action.DiceModificationName);
+                canBeUsed = false;
+            }
+            
+        }
+
+        private bool FilterTargets(GenericShip ship)
+        {
+            DistanceInfo distanceInfo = new DistanceInfo(ship, Combat.Defender);
+            return distanceInfo.Range < 2 && ship.Owner.PlayerNo == HostShip.Owner.PlayerNo;
+        }
+
+        private int GetFriendlyTargetPriority(GenericShip ship)
+        {
+            return 100-ship.PilotInfo.Cost;
         }
     }
 }

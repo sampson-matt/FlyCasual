5e414bf95765d1b590e27af54607b5632002ccca
sampson-matt <sampson.matt@gmail.com>
2022-12-17 09:09:58 -0600

    Added Slider pilot

--- Assets/Scripts/Model/Content/SecondEdition/Pilots/CloneZ95Headhunter/Slider.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/CloneZ95Headhunter/Slider.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Upgrade;
+using Movement;
 
 namespace Ship
 {
@@ -14,8 +15,6 @@ namespace Ship
         {
             public Slider() : base()
             {
-                RequiredMods = new List<Type>() { typeof(Mods.ModsList.UnreleasedContentMod) };
-
                 PilotInfo = new PilotCardInfo(
                     "\"Slider\"",
                     4,
@@ -23,6 +22,7 @@ namespace Ship
                     pilotTitle: "Evasive Aviator",
                     isLimited: true,
                     charges: 2,
+                    regensCharges: 1,
                     abilityType: typeof(Abilities.SecondEdition.SliderAbility),
                     extraUpgradeIcons: new List<UpgradeType>() { UpgradeType.Talent}
                 );
@@ -39,12 +39,52 @@ namespace Abilities.SecondEdition
     {
         public override void ActivateAbility()
         {
-
+            HostShip.OnManeuverIsRevealed += CheckRevealedManeuved;
         }
 
         public override void DeactivateAbility()
         {
+            HostShip.OnManeuverIsRevealed -= CheckRevealedManeuved;
+        }
 
+        private void CheckRevealedManeuved(GenericShip ship)
+        {
+            if (ship.State.Charges > 1
+                && ship.RevealedManeuver != null
+                && ship.RevealedManeuver.Bearing == Movement.ManeuverBearing.Bank
+                && ship.RevealedManeuver.Speed == 2)
+            {
+                RegisterAbilityTrigger(TriggerTypes.OnManeuverIsRevealed, RegisterSliderAbility);
+            }
+        }
+
+        private void RegisterSliderAbility(object sender, EventArgs e)
+        {
+            AskToUseAbility
+            (
+                HostShip.PilotInfo.PilotName,
+                NeverUseByDefault,
+                DoSideSlip,
+                descriptionLong: "Do you want to perform sideslip instead?",
+                imageHolder: HostShip
+            );
+        }
+
+        private void DoSideSlip(object sender, EventArgs e)
+        {
+            GenericMovement movement = new SideslipBankMovement(
+                HostShip.RevealedManeuver.Speed,
+                HostShip.RevealedManeuver.Direction,
+                ManeuverBearing.SideslipBank,
+                HostShip.RevealedManeuver.ColorComplexity
+            );
+
+            Messages.ShowInfo($"{HostShip.PilotInfo.PilotName}: Maneuver is changed to Sideslip");
+            HostShip.SetAssignedManeuver(movement);
+
+            HostShip.SpendCharges(2);
+
+            Triggers.FinishTrigger();
         }
     }
 }

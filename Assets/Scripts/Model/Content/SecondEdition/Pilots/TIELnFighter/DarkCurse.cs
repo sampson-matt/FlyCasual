47e64b083d602d1df1d181831d49b54cd4acd970
sampson-matt <sampson.matt@gmail.com>
2022-12-19 12:46:43 -0600

    Add Dark Curse BoY pilot

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/TIELnFighter/DarkCurse.cs
@@ -0,0 +1,58 @@
+﻿using Upgrade;
+using Ship;
+
+namespace Ship
+{
+    namespace SecondEdition.TIELnFighter
+    {
+        public class DarkCurse: TIELnFighter
+        {
+            public DarkCurse() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "\"Dark Curse\"",
+                    6,
+                    35,
+                    isLimited: true,
+                    extraUpgradeIcon: UpgradeType.Talent,
+                    abilityType: typeof(Abilities.SecondEdition.DarkCurseAbility)
+                );
+                ShipInfo.Hull++;
+                ShipInfo.UpgradeIcons.Upgrades.Remove(UpgradeType.Modification);
+
+                ImageUrl = "https://raw.githubusercontent.com/sampson-matt/FlyCasualLegacyCustomCards/main/BattleOfYavin/darkcurse-boy.png";
+            }
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class DarkCurseAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.OnAttackStartAsDefender += AddDarkCursePilotAbility;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnAttackStartAsDefender -= AddDarkCursePilotAbility;
+        }
+
+        private void AddDarkCursePilotAbility()
+        {
+            Combat.Attacker.OnTryAddAvailableDiceModification += UseDarkCurseRestriction;
+            Combat.Attacker.OnTryAddDiceModificationOpposite += UseDarkCurseRestriction;
+        }
+
+        private void UseDarkCurseRestriction(GenericShip ship, ActionsList.GenericAction diceModification, ref bool canBeUsed)
+        {
+            if (!diceModification.IsNotRealDiceModification)
+            {
+                Messages.ShowErrorToHuman(HostShip.PilotInfo.PilotName + ": "+ Combat.Attacker.PilotInfo.PilotName+ " is unable to modify dice");
+                canBeUsed = false;
+            }
+        }
+    }
+}

1527db051454a9f58630f1e67b9dc6aa07e3d7a9
sampson-matt <sampson.matt@gmail.com>
2022-12-19 12:27:50 -0600

    add BoY Darth Vader

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/TIEAdvancedX1/DarthVaderBOY.cs
@@ -0,0 +1,33 @@
+﻿using Abilities.SecondEdition;
+using ActionsList;
+using Ship;
+using System;
+using System.Collections.Generic;
+using Upgrade;
+
+namespace Ship
+{
+    namespace SecondEdition.TIEAdvancedX1
+    {
+        public class DarthVaderBOY : TIEAdvancedX1
+        {
+            public DarthVaderBOY() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "Darth Vader",
+                    6,
+                    73,
+                    isLimited: true,
+                    abilityType: typeof(DarthVaderDefenderAbility),
+                    force: 3,
+                    extraUpgradeIcon: UpgradeType.ForcePower
+                );
+                ShipInfo.Shields++;
+                ShipInfo.UpgradeIcons.Upgrades.Remove(UpgradeType.Modification);
+                ImageUrl = "https://raw.githubusercontent.com/sampson-matt/FlyCasualLegacyCustomCards/main/BattleOfYavin/darthvader-boy.png";
+                PilotNameCanonical = "darthvader-boy";
+                ModelInfo.SkinName = "Blue";
+            }
+        }
+    }
+}

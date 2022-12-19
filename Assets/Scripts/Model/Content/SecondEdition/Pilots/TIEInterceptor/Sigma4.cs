ff0bdca4a57ebdaf705e402f7c822792f31cbcbb
sampson-matt <sampson.matt@gmail.com>
2022-12-19 15:01:46 -0600

    add Sigma4 pilot

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/TIEInterceptor/Sigma4.cs
@@ -0,0 +1,99 @@
+﻿using Abilities.SecondEdition;
+using SubPhases;
+using Upgrade;
+using Ship;
+using System.Linq;
+using Abilities;
+using ActionsList;
+using Actions;
+using System;
+
+namespace Ship
+{
+    namespace SecondEdition.TIEInterceptor
+    {
+        public class Sigma4 : TIEInterceptor
+        {
+            public Sigma4() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "Sigma4",
+                    4,
+                    42,
+                    isLimited: true,
+                    charges: 2,
+                    abilityType: typeof(Sigma4Ability),
+                    extraUpgradeIcon: UpgradeType.Talent
+                );
+                ShipInfo.Hull++;
+                ShipInfo.UpgradeIcons.Upgrades.Remove(UpgradeType.Modification);
+                ShipInfo.UpgradeIcons.Upgrades.Remove(UpgradeType.Configuration);
+                AutoThrustersAbility oldAbility = (AutoThrustersAbility)ShipAbilities.First(n => n.GetType() == typeof(AutoThrustersAbility));
+                ShipAbilities.Remove(oldAbility);
+                ShipAbilities.Add(new SensitiveControlsRealAbility());
+                ImageUrl = "https://raw.githubusercontent.com/sampson-matt/FlyCasualLegacyCustomCards/main/BattleOfYavin/sigma4-boy.png";
+            }
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    //After you perform a barrel roll action, you may spend a charge to perform a  boost action.
+    public class Sigma4Ability : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.OnActionIsPerformed += CheckConditions;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnActionIsPerformed -= CheckConditions;
+        }
+
+        private void CheckConditions(GenericAction action)
+        {
+            if (action is BarrelRollAction && HostShip.State.Charges > 0)
+            {
+                HostShip.OnActionDecisionSubphaseEnd += PerformBoostAction;
+            }
+        }
+
+        private void PerformBoostAction(GenericShip ship)
+        {
+            HostShip.OnActionDecisionSubphaseEnd -= PerformBoostAction;
+            HostShip.BeforeActionIsPerformed += PayCost;
+
+            RegisterAbilityTrigger(TriggerTypes.OnFreeAction, AskPerformPerositionAction);
+        }
+
+        private void AskPerformPerositionAction(object sender, System.EventArgs e)
+        {
+            HostShip.AskPerformFreeAction(
+                new BoostAction(),
+                CleanUp,
+                descriptionShort: Name,
+                descriptionLong: "After you perform a Barrel Roll action, you may spend a charge to perform Boost action"
+            );
+        }
+
+        private void PayCost(GenericAction action, ref bool isFreeAction)
+        {
+            HostShip.BeforeActionIsPerformed -= PayCost;
+            RegisterAbilityTrigger(TriggerTypes.BeforeActionIsPerformed, SpendCharge);
+        }
+
+        private void SpendCharge(object sender, EventArgs e)
+        {
+            HostShip.SpendCharge();
+            Triggers.FinishTrigger();
+        }
+
+        private void CleanUp()
+        {
+            HostShip.BeforeActionIsPerformed -= PayCost;
+            Triggers.FinishTrigger();
+        }
+    }
+}

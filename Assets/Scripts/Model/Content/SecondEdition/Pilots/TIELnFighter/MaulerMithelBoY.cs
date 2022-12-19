4eb248b30df58ebc3f67705a1e4183f5a4c2a1fc
sampson-matt <sampson.matt@gmail.com>
2022-12-19 13:23:47 -0600

    add Mauler Mithel BoY pilot

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/TIELnFighter/MaulerMithelBoY.cs
@@ -0,0 +1,61 @@
+﻿using Upgrade;
+using Ship;
+using BoardTools;
+
+namespace Ship
+{
+    namespace SecondEdition.TIELnFighter
+    {
+        public class MaulerMithelBoY : TIELnFighter
+        {
+            public MaulerMithelBoY() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "\"Mauler\" Mithel",
+                    5,
+                    30,
+                    isLimited: true,
+                    abilityType: typeof(Abilities.SecondEdition.MaulerMithelBoYAbility),
+                    extraUpgradeIcon: UpgradeType.Talent
+                );
+                ShipInfo.Hull++;
+                ShipInfo.UpgradeIcons.Upgrades.Remove(UpgradeType.Modification);
+                PilotNameCanonical = "maulermithel-boy";
+                ImageUrl = "https://raw.githubusercontent.com/sampson-matt/FlyCasualLegacyCustomCards/main/BattleOfYavin/maulermithel-boy.png";
+            }
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class MaulerMithelBoYAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.OnShotStartAsAttacker += CheckConditionsOffense;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnShotStartAsAttacker -= CheckConditionsOffense;
+        }
+
+        private void CheckConditionsOffense()
+        {
+            int countLeft = Board.GetShipsInArcAtRange(HostShip, Arcs.ArcType.Left, new UnityEngine.Vector2(0, 1), Team.Type.Friendly).FindAll(s => s.PilotInfo.PilotName == "Darth Vader" || s.PilotInfo.PilotName == "\"Backstabber\"").Count;
+            int countRight = Board.GetShipsInArcAtRange(HostShip, Arcs.ArcType.Right, new UnityEngine.Vector2(0, 1), Team.Type.Friendly).FindAll(s => s.PilotInfo.PilotName == "Darth Vader" || s.PilotInfo.PilotName == "\"Backstabber\"").Count;
+            if (countLeft + countRight >= 1)
+            {
+                HostShip.AfterGotNumberOfAttackDice += RollExtraAttackDice;
+            }
+        }
+
+        private void RollExtraAttackDice(ref int count)
+        {
+            count++;
+            Messages.ShowInfo(HostShip.PilotInfo.PilotName + " gains +1 attack die");
+            HostShip.AfterGotNumberOfAttackDice -= RollExtraAttackDice;
+        }
+    }
+}

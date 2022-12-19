3ec1d26778e781eab23711127bd4a81ba95c26af
sampson-matt <sampson.matt@gmail.com>
2022-12-19 14:10:31 -0600

    add Iden Versio BoY pilot

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/TIEInterceptor/IdenVersioBoY.cs
@@ -0,0 +1,97 @@
+﻿using Abilities.SecondEdition;
+using SubPhases;
+using Upgrade;
+using Ship;
+
+namespace Ship
+{
+    namespace SecondEdition.TIEInterceptor
+    {
+        public class IdenVersioBoY : TIEInterceptor
+        {
+            public IdenVersioBoY() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "Iden Versio",
+                    4,
+                    64,
+                    isLimited: true,
+                    charges: 2,
+                    regensCharges: 1,
+                    abilityType: typeof(IdenVersioBoYAbility),
+                    extraUpgradeIcon: UpgradeType.Talent
+                );
+                ShipInfo.Shields++;
+                ShipInfo.UpgradeIcons.Upgrades.Remove(UpgradeType.Modification);
+                PilotNameCanonical = "idenversio-boy";
+                ImageUrl = "https://raw.githubusercontent.com/sampson-matt/FlyCasualLegacyCustomCards/main/BattleOfYavin/idenversio-boy.png";
+            }
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class IdenVersioBoYAbility : GenericAbility
+    {
+        private GenericShip curToDamage;
+
+        public override void ActivateAbility()
+        {
+            GenericShip.OnTryDamagePreventionGlobal += CheckIdenVersioAbilitySE;
+        }
+
+        public override void DeactivateAbility()
+        {
+            GenericShip.OnTryDamagePreventionGlobal -= CheckIdenVersioAbilitySE;
+        }
+
+        private void CheckIdenVersioAbilitySE(GenericShip toDamage, DamageSourceEventArgs e)
+        {
+            curToDamage = toDamage;
+
+            // Is the defender on our team? If not return.
+            if (curToDamage.Owner.PlayerNo != HostShip.Owner.PlayerNo)
+                return;
+
+            if (!(curToDamage is TIE))
+                return;
+
+            // If the defender is at range one of us we register our trigger to prevent damage.
+            BoardTools.DistanceInfo distanceInfo = new BoardTools.DistanceInfo(curToDamage, HostShip);
+            if (distanceInfo.Range <= 1)
+            {
+                RegisterAbilityTrigger(TriggerTypes.OnTryDamagePrevention, UseIdenVersioAbilitySE);
+            }
+        }
+
+        private void UseIdenVersioAbilitySE(object sender, System.EventArgs e)
+        {
+            // Are there any non-crit damage results in the damage queue?
+            if (HostShip.State.Charges > 1)
+            {
+                // If there are we prompt to see if they want to use the ability.
+                AskToUseAbility(
+                    HostShip.PilotInfo.PilotName,
+                    AlwaysUseByDefault,
+                    delegate { BlankDamage(); },
+                    descriptionLong: "Do you want to spend 2 Charges to prevent damage?",
+                    imageHolder: HostShip
+                );
+            }
+            else
+            {
+                Triggers.FinishTrigger();
+            }
+        }
+
+        private void BlankDamage()
+        {
+            HostShip.SpendCharges(2);
+            curToDamage.AssignedDamageDiceroll.RemoveAll();
+            DecisionSubPhase.ConfirmDecision();
+        }
+
+
+    }
+}

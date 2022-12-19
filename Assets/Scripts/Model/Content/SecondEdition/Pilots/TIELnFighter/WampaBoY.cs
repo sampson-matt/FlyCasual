97354df9353bad4279eabc098d3d4d15fb5a0d28
sampson-matt <sampson.matt@gmail.com>
2022-12-19 13:52:32 -0600

    Add Wampa BoY pilot

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/TIELnFighter/WampaBoY.cs
@@ -0,0 +1,31 @@
+﻿using Upgrade;
+using Abilities.SecondEdition;
+using Actions;
+using ActionsList;
+
+namespace Ship
+{
+    namespace SecondEdition.TIELnFighter
+    {
+        public class WampaBoY : TIELnFighter
+        {
+            public WampaBoY() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "\"Wampa\"",
+                    1,
+                    30,
+                    isLimited: true,
+                    abilityType: typeof(WampaAbility),
+                    charges: 1,
+                    regensCharges: 1
+                );
+                ShipInfo.ActionIcons.AddActions(new ActionInfo(typeof(TargetLockAction)));
+                ShipInfo.Hull++;
+                ShipInfo.UpgradeIcons.Upgrades.Remove(UpgradeType.Modification);
+                PilotNameCanonical = "wampa-boy";
+                ImageUrl = "https://raw.githubusercontent.com/sampson-matt/FlyCasualLegacyCustomCards/main/BattleOfYavin/wampa-boy.png";
+            }
+        }
+    }
+}

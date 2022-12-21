65924b31b2cb596cab7246c0d3f19b4a433ee311
sampson-matt <sampson.matt@gmail.com>
2022-12-21 08:11:21 -0600

    Add Luke and Wedge BoY pilots

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/T65XWing/LukeSkywalkerBoY.cs
@@ -0,0 +1,93 @@
+﻿using ActionsList;
+using BoardTools;
+using Ship;
+using SubPhases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Tokens;
+using Upgrade;
+using UpgradesList.SecondEdition;
+using Abilities.SecondEdition;
+
+namespace Ship
+{
+    namespace SecondEdition.T65XWing
+    {
+        public class LukeSkywalkerBoY : T65XWing
+        {
+            public LukeSkywalkerBoY() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "Luke Skywalker",
+                    5,
+                    62,
+                    isLimited: true,
+                    abilityType: typeof(LukeSkywalkerAbility),
+                    force: 2,
+                    extraUpgradeIcon: UpgradeType.ForcePower
+                );
+                ShipAbilities.Add(new HopeAbility());
+                ImageUrl = "https://raw.githubusercontent.com/sampson-matt/FlyCasualLegacyCustomCards/main/BattleOfYavin/lukeskywalker-boy.png";
+                PilotNameCanonical = "lukeskywalker-boy";
+                ModelInfo.SkinName = "Luke Skywalker";
+            }
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class HopeAbility : GenericAbility
+    {
+        private GenericShip PreviousCurrentShip { get; set; }
+
+        public override void ActivateAbility()
+        {
+            GenericShip.OnShipIsDestroyedGlobal += CheckAbility;
+        }
+
+        public override void DeactivateAbility()
+        {
+            GenericShip.OnShipIsDestroyedGlobal -= CheckAbility;
+        }
+
+        private void CheckAbility(GenericShip ship, bool flag)
+        {
+            if (!Tools.IsSameTeam(HostShip, ship) || Tools.IsSameShip(HostShip, ship)) return;
+            
+            DistanceInfo distanceInfo = new DistanceInfo(HostShip, ship);
+            if (distanceInfo.Range > 3) return;
+
+            RegisterAbilityTrigger(
+                TriggerTypes.OnShipIsDestroyed,
+                AskWhatToDo,
+                customTriggerName: $"{HostShip.PilotInfo.PilotName} (ID: {HostShip.ShipId})"
+            );
+        }
+
+        private void AskWhatToDo(object sender, EventArgs e)
+        {
+            PreviousCurrentShip = Selection.ThisShip;
+
+            Selection.ChangeActiveShip(HostShip);
+            Selection.ThisShip.AskPerformFreeAction(
+                new List<GenericAction>()
+                {
+                    new FocusAction(){ HostShip = HostShip },
+                    new BoostAction(){ HostShip = HostShip },
+                },
+                FinishAbility,
+                descriptionShort: HostShip.PilotInfo.PilotName,
+                descriptionLong: "You may perform a Focus or Boost action",
+                imageHolder: HostShip
+            );
+        }
+
+        private void FinishAbility()
+        {
+            Selection.ChangeActiveShip(PreviousCurrentShip);
+            Triggers.FinishTrigger();
+        }
+    }
+}
---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/T65XWing/WedgeAntillesBoY.cs
@@ -0,0 +1,66 @@
+﻿using Conditions;
+using Ship;
+using Upgrade;
+using Abilities.SecondEdition;
+
+namespace Ship
+{
+    namespace SecondEdition.T65XWing
+    {
+        public class WedgeAntillesBoY : T65XWing
+        {
+            public WedgeAntillesBoY() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "Wedge Antilles",
+                    5,
+                    48,
+                    isLimited: true,
+                    abilityType: typeof(Abilities.SecondEdition.WedgeAntillesBoYAbility),
+                    extraUpgradeIcon: UpgradeType.Talent,
+                    seImageNumber: 1
+                );
+                ShipInfo.UpgradeIcons.Upgrades.Remove(UpgradeType.Talent);
+                ShipAbilities.Add(new HopeAbility());
+                ImageUrl = "https://raw.githubusercontent.com/sampson-matt/FlyCasualLegacyCustomCards/main/BattleOfYavin/wedgeantilles-boy.png";
+                PilotNameCanonical = "wedgeantilles-boy";
+                ModelInfo.SkinName = "Wedge Antilles";
+            }
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class WedgeAntillesBoYAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.OnAttackStartAsAttacker += TryAddWedgeAntillesAbility;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnAttackStartAsAttacker -= TryAddWedgeAntillesAbility;
+        }
+
+        public void TryAddWedgeAntillesAbility()
+        {
+            bool friendlyShipInArc = false;
+            foreach (GenericShip friendlyShip in HostShip.Owner.Ships.Values)
+            {
+                if (friendlyShip.ShipId == HostShip.ShipId) return;
+                BoardTools.ShotInfo shotInfo = new BoardTools.ShotInfo(Combat.Defender, friendlyShip, Combat.Defender.PrimaryWeapons);
+                if (shotInfo.InArc) friendlyShipInArc = true;
+            }
+            if (Combat.ChosenWeapon.WeaponType == WeaponTypes.PrimaryWeapon && friendlyShipInArc)
+            {
+                if (HostShip.SectorsInfo.IsShipInSector(Combat.Defender, Arcs.ArcType.Front))
+                {
+                    WedgeAntillesCondition condition = new WedgeAntillesCondition(Combat.Defender, HostShip);
+                    Combat.Defender.Tokens.AssignCondition(condition);
+                }
+            }
+        }
+    }
+}

736817352904408d8f43b78ff876eefcb4f90909
sampson-matt <sampson.matt@gmail.com>
2022-12-21 16:05:06 -0600

    added Hol Okand BoY pilot

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/BTLA4YWing/HolOkandBoY.cs
@@ -0,0 +1,122 @@
+﻿using Abilities.SecondEdition;
+using Arcs;
+using Ship;
+using SubPhases;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using BoardTools;
+using UnityEngine;
+using Upgrade;
+
+namespace Ship
+{
+    namespace SecondEdition.BTLA4YWing
+    {
+        public class HolOkandBoY : BTLA4YWing
+        {
+            public HolOkandBoY() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "Hol Okand",
+                    4,
+                    36,
+                    pilotTitle: "Battle of Yavin",
+                    isLimited: true,
+                    abilityType: typeof(HolOkandBoYAbility),
+                    extraUpgradeIcons: new List<UpgradeType>() { UpgradeType.Modification }
+                );
+                ShipAbilities.Add(new HopeAbility());
+                ImageUrl = "https://raw.githubusercontent.com/sampson-matt/FlyCasualLegacyCustomCards/main/BattleOfYavin/holokand-boy.png";
+                PilotNameCanonical = "holokand-boy";
+            }
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class HolOkandBoYAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.OnCheckSystemsAbilityActivation += CheckForAbility;
+            HostShip.OnSystemsAbilityActivation += RegisterAbility;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnCheckSystemsAbilityActivation -= CheckForAbility;
+            HostShip.OnSystemsAbilityActivation -= RegisterAbility;
+        }
+
+        private void RegisterAbility(GenericShip ship)
+        {
+            if (HasUpgradeToRecharge() && Board.GetShipsAtRange(HostShip, new UnityEngine.Vector2(1, 2), Team.Type.Enemy).Count == 0)
+            {
+                RegisterAbilityTrigger(TriggerTypes.OnSystemsAbilityActivation, AskToUseHolOkandBoYAbility);
+            }
+        }
+
+        private void AskToUseHolOkandBoYAbility(object sender, EventArgs e)
+        {
+            HolOkandDecisionSubphase subphase = Phases.StartTemporarySubPhaseNew<HolOkandDecisionSubphase>("Hol Okand Subphase Decision", Triggers.FinishTrigger);
+
+            subphase.DescriptionShort = HostShip.PilotInfo.PilotName;
+            subphase.DescriptionLong = "You may recover one Charge on any upgrade";
+            subphase.ImageSource = HostShip;
+
+            foreach (GenericUpgrade upgrade in HostShip.UpgradeBar.GetUpgradesAll())
+            {
+                if (upgrade.State.MaxCharges > 0
+                    && upgrade.State.Charges < upgrade.State.MaxCharges
+                    && !upgrade.UpgradeInfo.CannotBeRecharged
+                )
+                {
+                    subphase.AddDecision(
+                        upgrade.UpgradeInfo.Name,
+                        delegate { RecoverUpgradeCharge(upgrade); },
+                        upgrade.ImageUrl
+                    );
+                }
+            }
+
+            subphase.ShowSkipButton = true;
+            subphase.DecisionOwner = HostShip.Owner;
+            subphase.DefaultDecisionName = subphase.GetDecisions().First()?.Name;
+
+            subphase.Start();
+        }
+
+        private void RecoverUpgradeCharge(GenericUpgrade upgrade)
+        {
+            DecisionSubPhase.ConfirmDecisionNoCallback();
+            upgrade.State.RestoreCharges(1);
+            Triggers.FinishTrigger();
+        }
+
+        private void CheckForAbility(GenericShip ship, ref bool flag)
+        {
+            if (HasUpgradeToRecharge() && Board.GetShipsAtRange(HostShip, new UnityEngine.Vector2(1, 2), Team.Type.Enemy).Count == 0) flag = true;
+        }
+
+        private bool HasUpgradeToRecharge()
+        {
+            foreach (GenericUpgrade upgrade in HostShip.UpgradeBar.GetUpgradesAll())
+            {
+                if (upgrade.State.MaxCharges > 0
+                    && upgrade.State.Charges < upgrade.State.MaxCharges
+                    && !upgrade.UpgradeInfo.CannotBeRecharged
+                )
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private class HolOkandDecisionSubphase : DecisionSubPhase { }
+    }
+}

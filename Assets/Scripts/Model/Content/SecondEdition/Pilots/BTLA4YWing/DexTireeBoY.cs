204874adc74fc157ac5a947a12e5a7e149fea4bb
sampson-matt <sampson.matt@gmail.com>
2022-12-21 14:22:22 -0600

    add Dex Tiree BoY pilot

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/BTLA4YWing/DexTireeBoY.cs
@@ -0,0 +1,58 @@
+﻿using Abilities.SecondEdition;
+using BoardTools;
+using Upgrade;
+
+namespace Ship
+{
+    namespace SecondEdition.BTLA4YWing
+    {
+        public class DexTireeBoY : BTLA4YWing
+        {
+            public DexTireeBoY() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "Dex Tiree",
+                    2,
+                    34,
+                    isLimited: true,
+                    abilityType: typeof(DexTireeAbility),
+                    extraUpgradeIcon: UpgradeType.Modification
+                );
+                ShipAbilities.Add(new HopeAbility());
+                ImageUrl = "https://raw.githubusercontent.com/sampson-matt/FlyCasualLegacyCustomCards/main/BattleOfYavin/dextiree-boy.png";
+                PilotNameCanonical = "dextiree-boy";
+            }
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class DexTireeAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.OnShotStartAsDefender += CheckConditionsDefense;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnShotStartAsDefender -= CheckConditionsDefense;
+        }
+
+        private void CheckConditionsDefense()
+        {
+            if (Board.GetShipsAtRange(HostShip, new UnityEngine.Vector2(0, 1), Team.Type.Friendly).Count > 1)
+            {
+                HostShip.AfterGotNumberOfDefenceDice += RollExtraDefenseDice;
+            }
+        }
+
+        private void RollExtraDefenseDice(ref int count)
+        {
+            count++;
+            Messages.ShowInfo(HostShip.PilotInfo.PilotName + " is within range 1 of another friendly ship and gains +1 defense die");
+            HostShip.AfterGotNumberOfDefenceDice -= RollExtraDefenseDice;
+        }
+    }
+}

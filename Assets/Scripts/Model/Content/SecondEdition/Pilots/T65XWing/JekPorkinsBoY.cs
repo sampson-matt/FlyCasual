a184fc5db621ff92c3cef9b557fb8f4aa9f127af
sampson-matt <sampson.matt@gmail.com>
2022-12-21 11:40:39 -0600

    add Jek Porkins BoY pilot

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/T65XWing/JekPorkinsBoY.cs
@@ -0,0 +1,27 @@
+﻿using Abilities.SecondEdition;
+using System;
+using Upgrade;
+
+namespace Ship
+{
+    namespace SecondEdition.T65XWing
+    {
+        public class JekPorkinsBoY : T65XWing
+        {
+            public JekPorkinsBoY() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "Jek Porkins",
+                    4,
+                    44,
+                    isLimited: true,
+                    abilityType: typeof(JekPorkinsAbility)
+                );
+                ShipAbilities.Add(new HopeAbility());
+                ImageUrl = "https://raw.githubusercontent.com/sampson-matt/FlyCasualLegacyCustomCards/main/BattleOfYavin/jekporkins-boy.png";
+                PilotNameCanonical = "jekporkins-boy";
+                ModelInfo.SkinName = "Jek Porkins";
+            }
+        }
+    }
+}

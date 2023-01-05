a61f47e166599f08a514c9b2453ab30a9db4fe67
sampson-matt <sampson.matt@gmail.com>
2023-01-05 11:22:03 -0600

    Add Contrail SoC and Klick SoC pilots

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/NimbusClassVWing/ContrailSoC.cs
@@ -0,0 +1,37 @@
+﻿using Ship;
+using Content;
+using System.Collections.Generic;
+using Upgrade;
+
+namespace Ship
+{
+    namespace SecondEdition.NimbusClassVWing
+    {
+        public class ContrailSoC : NimbusClassVWing
+        {
+            public ContrailSoC() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "\"Contrail\"",
+                    5,
+                    33,
+                    isLimited: true,
+                    abilityType: typeof(Abilities.SecondEdition.ContrailAbility),
+                    tags: new List<Tags>
+                    {
+                        Tags.SoC
+                    },
+                    extraUpgradeIcon: UpgradeType.Talent
+                );
+                ShipAbilities.Add(new Abilities.SecondEdition.BornForThisAbility());
+
+                ShipInfo.Tags.Remove(Tags.Tie);
+
+                PilotNameCanonical = "contrail-soc";
+
+                ImageUrl = "https://raw.githubusercontent.com/sampson-matt/FlyCasualLegacyCustomCards/main/SiegeOfCoruscant/contrail-soc.png";
+            }
+        }
+    }
+}
+
---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/NimbusClassVWing/KlickSoC.cs
@@ -0,0 +1,41 @@
+﻿using BoardTools;
+using Ship;
+using SubPhases;
+using Content;
+using System.Collections.Generic;
+using Upgrade;
+
+namespace Ship
+{
+    namespace SecondEdition.NimbusClassVWing
+    {
+        public class KlickSoC : NimbusClassVWing
+        {
+            public KlickSoC() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "\"Klick\"",
+                    4,
+                    37,
+                    isLimited: true,
+                    charges: 1,
+                    regensCharges: 1,
+                    abilityType: typeof(Abilities.SecondEdition.KlickAbility),
+                    tags: new List<Tags>
+                    {
+                        Tags.SoC
+                    },
+                    extraUpgradeIcon: UpgradeType.Talent
+                );
+                ShipInfo.Shields++;
+                ShipInfo.UpgradeIcons.Upgrades.Remove(UpgradeType.Modification);
+                ShipInfo.Tags.Remove(Tags.Tie);
+                ShipAbilities.Add(new Abilities.SecondEdition.BornForThisAbility());
+
+                PilotNameCanonical = "klick-soc";
+
+                ImageUrl = "https://raw.githubusercontent.com/sampson-matt/FlyCasualLegacyCustomCards/main/SiegeOfCoruscant/klick-soc.png";
+            }
+        }
+    }
+}

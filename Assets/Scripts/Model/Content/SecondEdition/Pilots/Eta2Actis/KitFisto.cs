a273e3bd4bd9c73cce37ccfa527195ec38ed02bd
sampson-matt <sampson.matt@gmail.com>
2023-01-09 08:28:46 -0600

    Add Kit Fisto Pilot

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/Eta2Actis/KitFisto.cs
@@ -0,0 +1,121 @@
+using BoardTools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Upgrade;
+using Tokens;
+
+namespace Ship.SecondEdition.Eta2Actis
+{
+    public class KitFisto : Eta2Actis
+    {
+        public KitFisto()
+        {
+            PilotInfo = new PilotCardInfo(
+                "Kit Fisto",
+                4,
+                43,
+                true,
+                force: 2,
+                abilityType: typeof(Abilities.SecondEdition.KitFistoActisAbility),
+                extraUpgradeIcon: UpgradeType.Talent,
+                pilotTitle: "Enthusiastic Exemplar"
+            );
+
+            ModelInfo.SkinName = "Red";
+
+            ImageUrl = "https://infinitearenas.com/xw2legacy/images/pilots/kitfisto.png";
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class KitFistoActisAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            AddDiceModification(
+                "Kit Fisto",
+                IsAvailable,
+                GetAiPriority,
+                DiceModificationType.Change,
+                count: 1,
+                sidesCanBeSelected: new List<DieSide>() { DieSide.Blank },
+                sideCanBeChangedTo: DieSide.Focus,
+                isGlobal: true,
+                payAbilityCost: PayForceCost
+            );
+        }
+
+        private bool IsAvailable()
+        {
+            bool result = false;
+
+            if (Combat.AttackStep == CombatStep.Defence && Combat.Defender.Owner.PlayerNo == HostShip.Owner.PlayerNo && HostShip.State.Force > 0)
+            {
+                ShotInfoArc arcInfo = new ShotInfoArc(
+                    Combat.Defender,
+                    Combat.Attacker,
+                    HostShip.SectorsInfo.Arcs.First(n => n.Facing == Arcs.ArcFacing.Bullseye)
+                );
+
+                if (arcInfo.InArc && arcInfo.Range <= 3) result = true;
+            }
+
+            return result;
+        }
+
+        private int GetAiPriority()
+        {
+            int result = 0;
+
+            //want to use it if we have a blank, and a way to use the blank, and a need to do so
+
+            if (Combat.AttackStep == CombatStep.Defence)
+            {
+                int attackSuccessesCancelable = Combat.DiceRollAttack.SuccessesCancelable;
+                int defenceSuccesses = Combat.CurrentDiceRoll.Successes;
+                //ship needs help
+                if (attackSuccessesCancelable > defenceSuccesses)
+                {
+                    //ship rolled a blank
+                    if(Combat.CurrentDiceRoll.Blanks > 0)
+                    {
+                        int numFocusTokens = Combat.Defender.Tokens.CountTokensByType(typeof(FocusToken));
+                        int numCalculateTokens = Combat.Defender.Tokens.CountTokensByType(typeof(CalculateToken));
+                        int numForce = Combat.Defender.State.Force;
+                        int ableToUseFocusCount = numFocusTokens + numCalculateTokens + numForce;
+                        // ship has a way to use the focus
+                        if (ableToUseFocusCount > 0)
+                        {
+                            result = 100;
+                        }
+                    }
+                }
+            }
+
+            if (Editions.Edition.Current is Editions.SecondEdition && Combat.CurrentDiceRoll.Failures == 0) return 0;
+
+            return result;
+        }
+
+        public override void DeactivateAbility()
+        {
+            RemoveDiceModification();
+        }
+
+        private void PayForceCost(Action<bool> callback)
+        {
+            if (HostShip.State.Force > 0)
+            {
+                IsAbilityUsed = true;
+                HostShip.State.SpendForce(1, delegate { callback(true); });
+            }
+            else
+            {
+                callback(false);
+            }
+        }
+    }
+}

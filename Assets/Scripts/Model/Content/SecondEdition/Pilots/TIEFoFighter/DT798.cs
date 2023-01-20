8d3e20ce3f7200f9f0e888956ba88ea6e9e4a9f4
sampson-matt <sampson.matt@gmail.com>
2023-01-20 14:17:13 -0600

    add DT798 pilot

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/TIEFoFighter/DT798.cs
@@ -0,0 +1,93 @@
+﻿using ActionsList;
+using Ship;
+using SubPhases;
+using System;
+using Upgrade;
+
+namespace Ship
+{
+    namespace SecondEdition.TIEFoFighter
+    {
+        public class DT798 : TIEFoFighter
+        {
+            public DT798() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "DT-798",
+                    4,
+                    34,
+                    pilotTitle: "Jace Rucklin",
+                    isLimited: true,
+                    abilityType: typeof(Abilities.SecondEdition.DT798PilotAbility),
+                    extraUpgradeIcon: UpgradeType.Talent
+                );
+
+                ImageUrl = "https://infinitearenas.com/xw2legacy/images/pilots/dt798.png";
+            }
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class DT798PilotAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.OnShotStartAsAttacker += CheckAbilityTrigger;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnShotStartAsAttacker -= CheckAbilityTrigger;
+        }
+
+        private void CheckAbilityTrigger()
+        {
+            if (IsAvailable())
+            {
+                RegisterAbilityTrigger(TriggerTypes.OnShotStart, AskUseAbility);
+            }
+        }
+
+        private bool IsAvailable()
+        {
+            if (HostShip.IsStrained) return false;
+            if (Combat.ChosenWeapon.WeaponType != WeaponTypes.PrimaryWeapon) return false;
+
+            return true;
+        }
+
+        private void AskUseAbility(object sender, EventArgs e)
+        {
+            AskToUseAbility(
+                HostShip.PilotInfo.PilotName,
+                AlwaysUseByDefault,
+                UseAbilityDecision,
+                descriptionLong: "Do you want to gain 1 Strain to roll 1 additional attack die?",
+                imageHolder: HostShip
+            );
+        }
+
+        private void UseAbilityDecision(object sender, EventArgs e)
+        {
+            AllowRollAdditionalDie();
+            HostShip.Tokens.AssignToken(
+                typeof(Tokens.StrainToken),
+                DecisionSubPhase.ConfirmDecision
+            );
+        }
+
+        private void AllowRollAdditionalDie()
+        {
+            HostShip.AfterGotNumberOfAttackDice += RollExtraDie;
+        }
+
+        protected void RollExtraDie(ref int diceCount)
+        {
+            HostShip.AfterGotNumberOfAttackDice -= RollExtraDie;
+            Messages.ShowInfo(HostName + ": +1 attack die");
+            diceCount++;
+        }
+    }
+}

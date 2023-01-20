6e15b1b2c104a00bf78d4b5815a12e913f1d86fa
sampson-matt <sampson.matt@gmail.com>
2023-01-20 12:38:14 -0600

    add Zay Versio pilot

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/T70XWing/ZayVersio.cs
@@ -0,0 +1,62 @@
+﻿using Movement;
+using Ship;
+using Tokens;
+using Upgrade;
+
+namespace Ship
+{
+    namespace SecondEdition.T70XWing
+    {
+        public class ZayVersio : T70XWing
+        {
+            public ZayVersio() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "Zay Versio",
+                    3,
+                    45,
+                    pilotTitle: "Her Father's Daughter",
+                    isLimited: true,
+                    abilityType: typeof(Abilities.SecondEdition.ZayVersioAbility),
+                    extraUpgradeIcon: UpgradeType.Talent
+                );
+
+                ImageUrl = "https://infinitearenas.com/xw2legacy/images/pilots/zayversio.png";
+            }
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class ZayVersioAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            AddDiceModification(
+                HostName,
+                IsAvailable,
+                GetAiPriority,
+                DiceModificationType.Reroll,
+                1
+                );
+        }
+
+        public override void DeactivateAbility()
+        {
+            RemoveDiceModification();
+        }
+
+        private int GetAiPriority()
+        {
+            return 90;
+        }
+
+        private bool IsAvailable()
+        {
+            return Combat.Defender == HostShip
+                && Combat.AttackStep == CombatStep.Defence
+                && Combat.Attacker.Damage.IsDamaged;
+        }
+    }
+}

d581a6d03933ed6cf55dd474779e9042fb626401
sampson-matt <sampson.matt@gmail.com>
2023-02-01 07:05:01 -0600

    fix Quick Draw pilot cost

--- Assets/Scripts/Model/Content/SecondEdition/Pilots/TIESfFighter/QuickDraw.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/TIESfFighter/QuickDraw.cs
@@ -13,7 +13,7 @@ namespace Ship
                 PilotInfo = new PilotCardInfo(
                     "\"Quickdraw\"",
                     6,
-                    47,
+                    44,
                     isLimited: true,
                     extraUpgradeIcon: UpgradeType.Talent,
                     abilityType: typeof(Abilities.SecondEdition.QuickdrawAbility),

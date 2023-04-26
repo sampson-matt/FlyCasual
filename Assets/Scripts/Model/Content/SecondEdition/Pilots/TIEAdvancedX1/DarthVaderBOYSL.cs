d491d3637ea6dc182c5fc127d1fd73ccc27dfee6
sampson-matt <sampson.matt@gmail.com>
2023-04-26 12:27:09 -0500

    Add Darth Vader SL pilot

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/TIEAdvancedX1/DarthVaderBOYSL.cs
@@ -0,0 +1,45 @@
+﻿using Abilities.SecondEdition;
+using System;
+using Ship;
+using Content;
+using System.Collections.Generic;
+using Upgrade;
+using UpgradesList.SecondEdition;
+
+namespace Ship
+{
+    namespace SecondEdition.TIEAdvancedX1
+    {
+        public class DarthVaderBOYSL : TIEAdvancedX1
+        {
+            public DarthVaderBOYSL() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "Darth Vader",
+                    6,
+                    81,
+                    isLimited: true,
+                    abilityType: typeof(DarthVaderBoYAbility),
+                    force: 3,
+                    tags: new List<Tags>
+                    {
+                        Tags.BoY,
+                        Tags.DarkSide,
+                        Tags.Sith
+                    },
+                    extraUpgradeIcon: UpgradeType.ForcePower,
+                    isStandardLayout: true
+                );
+                ShipInfo.Shields++;
+                ShipInfo.UpgradeIcons.Upgrades.Remove(UpgradeType.Modification);
+                ImageUrl = "https://static.wikia.nocookie.net/xwing-miniatures-second-edition/images/a/a9/Darthvader-battleofyavin.png";
+                PilotNameCanonical = "darthvader-battleofyavin-sl";
+                ModelInfo.SkinName = "Blue";
+
+                MustHaveUpgrades.Add(typeof(Marksmanship));
+                MustHaveUpgrades.Add(typeof(Hate));
+                MustHaveUpgrades.Add(typeof(AfterBurners));
+            }
+        }
+    }
+}
--- Assets/Scripts/Model/SquadBuilder/SquadLists/SquadJsonHelper.cs
+++ Assets/Scripts/Model/SquadBuilder/SquadLists/SquadJsonHelper.cs
@@ -107,18 +107,21 @@ namespace SquadBuilderNS
             pilotJson.AddField("ship", shipHolder.Instance.ShipTypeCanonical);
 
             Dictionary<string, JSONObject> upgradesDict = new Dictionary<string, JSONObject>();
-            foreach (var installedUpgrade in shipHolder.Instance.UpgradeBar.GetUpgradesAll())
+            if (!shipHolder.Instance.PilotInfo.IsStandardLayout)
             {
-                string slotName = Edition.Current.UpgradeTypeToXws(installedUpgrade.UpgradeInfo.UpgradeTypes[0]);
-                if (!upgradesDict.ContainsKey(slotName))
+                foreach (var installedUpgrade in shipHolder.Instance.UpgradeBar.GetUpgradesAll())
                 {
-                    JSONObject upgrade = new JSONObject();
-                    upgrade.Add(installedUpgrade.NameCanonical);
-                    upgradesDict.Add(slotName, upgrade);
-                }
-                else
-                {
-                    upgradesDict[slotName].Add(installedUpgrade.NameCanonical);
+                    string slotName = Edition.Current.UpgradeTypeToXws(installedUpgrade.UpgradeInfo.UpgradeTypes[0]);
+                    if (!upgradesDict.ContainsKey(slotName))
+                    {
+                        JSONObject upgrade = new JSONObject();
+                        upgrade.Add(installedUpgrade.NameCanonical);
+                        upgradesDict.Add(slotName, upgrade);
+                    }
+                    else
+                    {
+                        upgradesDict[slotName].Add(installedUpgrade.NameCanonical);
+                    }
                 }
             }
             JSONObject upgradesDictJson = new JSONObject(upgradesDict);
@@ -257,6 +260,19 @@ namespace SquadBuilderNS
 
                                     if (!wasSuccess) break;
                                 }
+                            }
+                        }
+
+                        if (newShipInstance.PilotInfo.IsStandardLayout)
+                        {
+                            foreach (Type upgradeType in newShipInstance.MustHaveUpgrades)
+                            {
+                                bool upgradeInstalledSucessfully = newShip.InstallUpgrade(upgradeType.ToString());
+                                if (!upgradeInstalledSucessfully)
+                                {
+                                    Messages.ShowError("Cannot install upgrade: " + upgradeType.ToString());
+                                }
+
                             }
                         }
 

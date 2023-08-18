462b0ce421f9a5a0d8c154ffc6b70f469a6f9af9
sampson-matt <sampson.matt@gmail.com>
2023-08-18 13:39:11 -0500

    Add Maul crew upgrade

---
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Crew/MaulMandalore.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Upgrade;
+using ActionsList;
+using Content;
+using SquadBuilderNS;
+using UnityEngine;
+
+namespace UpgradesList.SecondEdition
+{
+    public class MaulMandalore : GenericUpgrade
+    {
+        public MaulMandalore() : base()
+        {
+            UpgradeInfo = new UpgradeCardInfo(
+                "Maul",
+                types: new List<UpgradeType>()
+                {
+                    UpgradeType.Crew,
+                    UpgradeType.Crew
+                },
+                cost: 10,
+                isLimited: true,
+                addForce: 1,
+                addSlot: new UpgradeSlot(UpgradeType.Illicit),
+                addAction: new Actions.ActionInfo(typeof(CoordinateAction), Actions.ActionColor.Purple),
+                restriction: new FactionRestriction(Faction.Scum)
+            );
+
+            NameCanonical = "maul-crew";
+
+        }
+
+    }
+}

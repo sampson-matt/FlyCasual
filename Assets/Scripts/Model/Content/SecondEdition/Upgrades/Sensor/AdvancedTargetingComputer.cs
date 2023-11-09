a1d6b969773e181f1b395905b31ae59adba41bc3
sampson-matt <sampson.matt@gmail.com>
2023-11-08 21:27:33 -0600

    add advanced targeting computer hotac upgrade

---
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Sensor/AdvancedTargetingComputer.cs
@@ -0,0 +1,24 @@
+﻿using Ship;
+using Upgrade;
+using ActionsList;
+using System;
+using System.Collections.Generic;
+using Tokens;
+
+namespace UpgradesList.SecondEdition
+{
+    public class AdvancedTargetingComputer : GenericUpgrade
+    {
+        public AdvancedTargetingComputer() : base()
+        {
+            UpgradeInfo = new UpgradeCardInfo(
+                "Advanced Targeting Computer",
+                UpgradeType.Sensor,
+                cost: 5,
+                abilityType: typeof(Abilities.FirstEdition.AdvancedTargetingComputerAbility)
+            );
+
+            ImageUrl = "https://raw.githubusercontent.com/sampson-matt/Hotac-Upgrade-Cards/main/Upgrades/sensor/advancedtargetingcomputer.png";
+        }
+    }
+}

ce4338b12cebec1688aca228da7c6dbbabcdfd59
sampson-matt <sampson.matt@gmail.com>
2024-07-29 08:09:55 -0500

    Update Rose Tico, allow to choose which die

--- Assets/Scripts/Model/Content/SecondEdition/Upgrades/Crew/RoseTico.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Crew/RoseTico.cs
@@ -2,6 +2,9 @@
 using Upgrade;
 using ActionsList;
 using UnityEngine;
+using Players;
+using SubPhases;
+using System.Collections.Generic;
 
 namespace UpgradesList.SecondEdition
 {
@@ -83,15 +86,87 @@ namespace ActionsList.SecondEdition
             return result;
         }
 
-        public override void ActionEffect(System.Action callBack)
+        public override void ActionEffect(System.Action callback)
         {
-            DieSide worstDieSide = Combat.CurrentDiceRoll.WorstResult;
-            Combat.CurrentDiceRoll.RemoveType(worstDieSide);
+            if (HostShip.Owner is HumanPlayer)
+            {
+                Triggers.RegisterTrigger(
+                    new Trigger()
+                    {
+                        Name = DiceModificationName,
+                        TriggerOwner = HostShip.Owner.PlayerNo,
+                        TriggerType = TriggerTypes.OnAbilityDirect,
+                        EventHandler = StartSubphase
+                    }
+                );
 
-            GenericShip opponentShip = (Combat.AttackStep == CombatStep.Attack) ? Combat.Defender : Combat.Attacker;
-            ActionsHolder.AcquireTargetLock(HostShip, opponentShip, callBack, callBack);
+                Triggers.ResolveTriggers(TriggerTypes.OnAbilityDirect, callback);
+            }
+            else
+            {
+                DieSide worstDieSide = Combat.CurrentDiceRoll.WorstResult;
+                Combat.CurrentDiceRoll.RemoveType(worstDieSide);
+                GenericShip opponentShip = (Combat.AttackStep == CombatStep.Attack) ? Combat.Defender : Combat.Attacker;
+                ActionsHolder.AcquireTargetLock(HostShip, opponentShip, callback, callback);
+            }
         }
 
+        private void StartSubphase(object sender, System.EventArgs e)
+        {
+            var spendDiceSubPhase = Phases.StartTemporarySubPhaseNew<RoseTicoDecisionSubPhase>(Name, Triggers.FinishTrigger);
+            spendDiceSubPhase.HostShip = HostShip;
+            spendDiceSubPhase.ShowSkipButton = true;
+            spendDiceSubPhase.OnSkipButtonIsPressed = DecisionSubPhase.ConfirmDecision;
+            spendDiceSubPhase.DecisionOwner = HostShip.Owner;
+            spendDiceSubPhase.Start();
+        }
     }
+}
 
+namespace SubPhases
+{
+    public class RoseTicoDecisionSubPhase : SpendDiceResultDecisionSubPhase
+    {
+        public GenericShip HostShip;
+
+        protected override void PrepareDiceResultEffects()
+        {
+            DescriptionShort = "Rose Tico";
+            DescriptionLong = "Select a die result to spend to acquire a lock";
+            ImageSource = HostShip;
+
+            if(Combat.AttackStep == CombatStep.Attack)
+            {
+                AddSpendDiceResultEffect(DieSide.Crit, "Critical result", delegate { SpendResultToLock(DieSide.Crit); });
+                AddSpendDiceResultEffect(DieSide.Success, "Hit result", delegate { SpendResultToLock(DieSide.Success); });
+                AddSpendDiceResultEffect(DieSide.Focus, "Focus result", delegate { SpendResultToLock(DieSide.Focus); });
+                AddSpendDiceResultEffect(DieSide.Blank, "Blank result", delegate { SpendResultToLock(DieSide.Blank); });
+            } 
+            else
+            {
+                AddSpendDiceResultEffect(DieSide.Success, "Evade result", delegate { SpendResultToLock(DieSide.Success); });
+                AddSpendDiceResultEffect(DieSide.Focus, "Focus result", delegate { SpendResultToLock(DieSide.Focus); });
+                AddSpendDiceResultEffect(DieSide.Blank, "Blank result", delegate { SpendResultToLock(DieSide.Blank); });
+            }
+            
+        }
+
+        private void SpendResultToLock(DieSide side)
+        {
+            DecisionSubPhase.ConfirmDecisionNoCallback();
+            if (Combat.AttackStep == CombatStep.Attack)
+            {
+                Combat.DiceRollAttack.RemoveType(side);
+                Combat.DiceRollAttack.OrganizeDicePositions();
+            }
+            else
+            {
+                Combat.DiceRollDefence.RemoveType(side);
+                Combat.DiceRollDefence.OrganizeDicePositions();
+            }
+
+            GenericShip opponentShip = (Combat.AttackStep == CombatStep.Attack) ? Combat.Defender : Combat.Attacker;
+            ActionsHolder.AcquireTargetLock(HostShip, opponentShip, Triggers.FinishTrigger, Triggers.FinishTrigger);
+        }
+    }
 }

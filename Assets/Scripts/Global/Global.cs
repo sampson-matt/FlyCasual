bf6cf53145f7ea7a2259d3cd5ef30680920120e2
sampson-matt <sampson.matt@gmail.com>
2024-07-29 08:22:44 -0500

    update version

--- Assets/Scripts/Global/Global.cs
+++ Assets/Scripts/Global/Global.cs
@@ -12,9 +12,9 @@ public class Global : MonoBehaviour {
 
     private static bool isAlreadyInitialized;
 
-    public static string CurrentVersion = "v0.28";
-    public static int CurrentVersionInt = 000028000;
-    public static int LatestVersionInt = 000028000;
+    public static string CurrentVersion = "v0.29";
+    public static int CurrentVersionInt = 000029000;
+    public static int LatestVersionInt = 000029000;
 
     public static SquadBuilder SquadBuilder { get; set;}
 

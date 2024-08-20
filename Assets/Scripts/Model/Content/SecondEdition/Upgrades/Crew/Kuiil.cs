9dc62004e80541550ba4cbf6697a5ce474c117eb
sampson-matt <sampson.matt@gmail.com>
2024-08-20 14:27:51 -0500

    Add Kuiil crew

--- Assets/Scripts/Model/Content/SecondEdition/Upgrades/Crew/Kuiil.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Crew/Kuiil.cs
@@ -1,4 +1,5 @@
-﻿using Ship;
+﻿using ActionsList;
+using Ship;
 using SubPhases;
 using System;
 using System.Collections.Generic;
@@ -13,8 +14,6 @@ namespace UpgradesList.SecondEdition
     {
         public Kuiil() : base()
         {
-            IsWIP = true;
-
             UpgradeInfo = new UpgradeCardInfo(
                 "Kuiil",
                 UpgradeType.Crew,
@@ -23,8 +22,6 @@ namespace UpgradesList.SecondEdition
                 restriction: new FactionRestriction(Faction.Scum),
                 abilityType: typeof(Abilities.SecondEdition.KuiilAbility)
             );
-
-            ImageUrl = "https://infinitearenas.com/xw2/images/upgrades/kuiil.png";
         }
     }
 }
@@ -38,9 +35,8 @@ namespace Abilities.SecondEdition
     //then for each icon: action focus result, gain 1 focus token. 
     public class KuiilAbility : GenericAbility
     {
-        public List<GenericToken> orangeTokens = new List<GenericToken>();
-
         public int hitIndex = 0;
+        public int blankIndex = 0;
 
         public override void ActivateAbility()
         {
@@ -70,28 +66,36 @@ namespace Abilities.SecondEdition
                 HostName,
                 DiceKind.Attack,
                 HostShip.Damage.DamageCards.Count,
-                DiceCheckFinished,
+                CheckSuccesses,
                 Phases.CurrentSubPhase.CallBack
             );
         }
-        protected void DiceCheckFinished()
+
+        private void CheckSuccesses()
         {
-            if (DiceCheckRoll.Successes > 0 && HostShip.Damage.GetFaceupCrits(CriticalCardType.Ship).Any())
+            if (DiceCheckRoll.RegularSuccesses > 0 && HostShip.Damage.GetFaceupCrits(CriticalCardType.Ship).Any())
             {
-                if (DiceCheckRoll.Successes >= HostShip.Damage.GetFaceupCrits(CriticalCardType.Ship).Count)
+                if (DiceCheckRoll.RegularSuccesses >= HostShip.Damage.GetFaceupCrits(CriticalCardType.Ship).Count)
                 {
-                    foreach(GenericDamageCard card in HostShip.Damage.GetFaceupCrits(CriticalCardType.Ship))
+                    foreach (GenericDamageCard card in HostShip.Damage.GetFaceupCrits(CriticalCardType.Ship))
                     {
                         HostShip.Damage.FlipFaceupCritFacedown(card, delegate { });
-                        Messages.ShowInfo(card.Name + " repaired");
                     }
+                    CheckCrits(AbilityDiceCheck.ConfirmCheck);
                 }
                 else
                 {
-                    hitIndex = DiceCheckRoll.Successes;
-                    ChooseRepairFaceupCrits();
+                    hitIndex = DiceCheckRoll.RegularSuccesses;
+                    ChooseRepairFaceupCrits(AbilityDiceCheck.ConfirmCheck);
                 }
             }
+            else
+            {
+                CheckCrits(AbilityDiceCheck.ConfirmCheck);
+            }
+        }
+        private void CheckCrits(Action callBack)
+        {
             if (DiceCheckRoll.CriticalSuccesses > 0 && HostShip.Damage.HasFacedownCards)
             {
                 for (int i = 0; i < DiceCheckRoll.CriticalSuccesses; i++)
@@ -100,63 +104,139 @@ namespace Abilities.SecondEdition
                     Messages.ShowInfo("Facedown damage card repaired.");
                 }
             }
+            CheckBlanks(callBack);
+        }
+        private void CheckBlanks(Action callBack)
+        {
             if (DiceCheckRoll.Blanks > 0 && HostShip.Tokens.CountTokensByColor(TokenColors.Orange) > 0)
             {
-                if(HostShip.Tokens.CountTokensByColor(TokenColors.Orange) <= DiceCheckRoll.Blanks)
+                if (HostShip.Tokens.CountTokensByColor(TokenColors.Orange) <= DiceCheckRoll.Blanks)
                 {
-                    HostShip.Tokens.RemoveAllTokensByColor(TokenColors.Orange, delegate { });
+                    HostShip.Tokens.RemoveAllTokensByColor(TokenColors.Orange, () => CheckFocuses(callBack));
                     Messages.ShowInfo("Orange Tokens Removed");
                 }
-
+                else
+                {
+                    blankIndex = DiceCheckRoll.Blanks;
+                    ChooseRemoveOrangeTokens(callBack);
+                }
             }
+            else
+            {
+                CheckFocuses(callBack);
+            }            
+        }
+        private void CheckFocuses(Action callBack)
+        {
             if (DiceCheckRoll.Focuses > 0)
             {
-                HostShip.Tokens.AssignTokens(CreateFocusToken, DiceCheckRoll.Focuses, delegate { });
-            }
-            AbilityDiceCheck.ConfirmCheck();
-        }
-
-        private void ChooseRepairFaceupCrits()
-        {
-            KuiilCritDecisonSubphase subphase = Phases.StartTemporarySubPhaseNew<KuiilCritDecisonSubphase>(
-                "Kuiil crit repair decision",
-                Phases.CurrentSubPhase.CallBack
-            );
-
-            subphase.DescriptionShort = HostShip.PilotInfo.PilotName;
-            subphase.DescriptionLong = "You may repair one faceup Ship damage card.";
-            subphase.ImageSource = HostUpgrade;
-
-            foreach (GenericDamageCard faceupCrit in HostShip.Damage.GetFaceupCrits(CriticalCardType.Ship))
+                HostShip.Tokens.AssignTokens(CreateFocusToken, DiceCheckRoll.Focuses, callBack);
+            } else
             {
-                subphase.AddDecision(faceupCrit.Name, delegate { Repair(faceupCrit); }, faceupCrit.ImageUrl);
-            }
-            subphase.DefaultDecisionName = subphase.GetDecisions().First().Name;
-
-
-            subphase.DecisionOwner = HostShip.Owner;
-
-            subphase.Start();
+                callBack();
+            }            
         }
-
-        private void Repair(GenericDamageCard critCard)
-        {
-            HostShip.Damage.FlipFaceupCritFacedown(critCard, DecisionSubPhase.ConfirmDecision);
-           // DecisionSubPhase.ConfirmDecision();
-            //hitIndex--;
-            //if(hitIndex >0)
-            //{
-            //    ChooseRepairFaceupCrits();
-            //}
-        }
-
         private GenericToken CreateFocusToken()
         {
             return new Tokens.FocusToken(HostShip);
+        }        
+        private void ChooseRepairFaceupCrits(Action callBack)
+        {
+            Action nextPhase = null;
+            if(hitIndex == 1)
+            {
+                nextPhase = () => CheckCrits(callBack);
+            } else
+            {
+                hitIndex--;
+                nextPhase = () => ChooseRepairFaceupCrits(callBack);
+            }
+
+            KuiilCritDecisonSubphase subphase = Phases.StartTemporarySubPhaseNew<KuiilCritDecisonSubphase>(
+                "Kuiil crit repair decision",
+                nextPhase
+            );
+
+            subphase.DescriptionShort = HostUpgrade.UpgradeInfo.Name;
+            subphase.DescriptionLong = "You may repair one faceup Ship damage card.";
+            subphase.ImageSource = HostUpgrade;
+            subphase.HostShip = HostShip;
+            subphase.DecisionOwner = HostShip.Owner;
+            subphase.Start();
+        }
+        private void ChooseRemoveOrangeTokens(Action callBack)
+        {
+            Action nextPhase = null;
+            if (blankIndex == 1)
+            {
+                nextPhase = () => CheckFocuses(callBack);
+            }
+            else
+            {
+                blankIndex--;
+                nextPhase = () => ChooseRemoveOrangeTokens(callBack);
+            }
+
+            KuiilBlankDecisonSubphase subphase = Phases.StartTemporarySubPhaseNew<KuiilBlankDecisonSubphase>(
+                "Kuiil remove orange token decision",
+                nextPhase
+            );
+
+            subphase.DescriptionShort = HostUpgrade.UpgradeInfo.Name;
+            subphase.DescriptionLong = "You may remove one orange token.";
+            subphase.ImageSource = HostUpgrade;
+            subphase.HostShip = HostShip;
+            subphase.DecisionOwner = HostShip.Owner;
+            subphase.Start();
+        }
+        private class KuiilCritDecisonSubphase : DecisionSubPhase
+        {
+            public GenericShip HostShip { get; set; }
+            public override void PrepareDecision(System.Action callBack)
+            {
+                DecisionViewType = DecisionViewTypes.ImagesDamageCard;
+
+                foreach (GenericDamageCard faceupCrit in HostShip.Damage.GetFaceupCrits(CriticalCardType.Ship))
+                {
+                    AddDecision(faceupCrit.Name, delegate { Repair(faceupCrit); }, faceupCrit.ImageUrl);
+                }
+                DefaultDecisionName = GetDecisions().First().Name;
+                callBack();
+            }
+            private void Repair(GenericDamageCard critCard)
+            {
+                HostShip.Damage.FlipFaceupCritFacedown(critCard, DecisionSubPhase.ConfirmDecision);
+            }
         }
 
-        private class KuiilCritDecisonSubphase : DecisionSubPhase { }
+        private class KuiilBlankDecisonSubphase : DecisionSubPhase
+        {
+            public GenericShip HostShip { get; set; }
+            public override void PrepareDecision(System.Action callBack)
+            {
+                DecisionViewType = DecisionViewTypes.TextButtons;
 
-
+                foreach (GenericToken token in HostShip.Tokens.GetTokensByColor(TokenColors.Orange))
+                {
+                    if (!GetDecisions().Any(n => n.Name == GetRemoveTokenDescription(token)))
+                    {
+                        AddDecision(
+                            GetRemoveTokenDescription(token),
+                            delegate { RemoveToken(token); }
+                        );
+                    }
+                }
+                DefaultDecisionName = GetDecisions().First().Name;
+                callBack();
+            }
+            private string GetRemoveTokenDescription(GenericToken token)
+            {
+                return $"Remove {token.Name}";
+            }
+            private void RemoveToken(GenericToken token)
+            {
+                HostShip.Tokens.RemoveToken(token, DecisionSubPhase.ConfirmDecision);
+            }
+        }
     }
 }

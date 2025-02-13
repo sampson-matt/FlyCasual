afd68ba4ced30b7ecc9cbb9195a27d134ebdaa82
sampson-matt <sampson.matt@gmail.com>
2025-02-13 11:21:58 -0600

    fix battle meditation cost

--- Assets/Scripts/Model/Content/SecondEdition/Upgrades/ForcePower/BattleMeditation.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/ForcePower/BattleMeditation.cs
@@ -28,11 +28,11 @@ namespace UpgradesList.SecondEdition
         {
             Dictionary<int, int> initiativeToCost = new Dictionary<int, int>()
             {
-                {0, 1},
-                {1, 1},
-                {2, 1},
-                {3, 1},
-                {4, 2},
+                {0, 2},
+                {1, 2},
+                {2, 2},
+                {3, 2},
+                {4, 3},
                 {5, 4},
                 {6, 6}
             };

6565f0cad2fe108a57c35fc4f993a9e7da35a57e
sampson-matt <sampson.matt@gmail.com>
2025-07-12 14:49:36 -0500

    fix Iron Assembler

--- Assets/Scripts/Model/Content/SecondEdition/Pilots/VultureClassDroidFighter/TheIronAssembler.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/VultureClassDroidFighter/TheIronAssembler.cs
@@ -52,7 +52,7 @@ namespace Abilities.SecondEdition
         {
             bool obstacleCheck = ship.ObstaclesLanded.Any(n => n.GetTypeName == "Asteroid" || n.GetTypeName == "Debris");
 
-            if (ship.IsManeuverSkipped && obstacleCheck && Tools.IsFriendly(ship, HostShip)
+            if (HostShip.State.Charges > 0 && ship.IsManeuverSkipped && obstacleCheck && Tools.IsFriendly(ship, HostShip)
                 && new DistanceInfo(ship, HostShip).Range < 2 && ship.Damage.IsDamaged)
             {
                 TargetShip = ship;

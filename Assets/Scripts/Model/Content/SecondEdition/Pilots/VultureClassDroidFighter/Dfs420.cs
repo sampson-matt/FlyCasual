d0f7f9ffacd6f00a0069d2522790af4e18b74b55
sampson-matt <sampson.matt@gmail.com>
2025-09-12 13:02:48 -0500

    Add DFS420

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/VultureClassDroidFighter/Dfs420.cs
@@ -0,0 +1,101 @@
+﻿using Content;
+using BoardTools;
+using ActionsList;
+using Ship;
+using System.Linq;
+using Movement;
+using System.Collections.Generic;
+using Tokens;
+using SubPhases;
+using Upgrade;
+
+namespace Ship.SecondEdition.VultureClassDroidFighter
+{
+    public class Dfs420 : VultureClassDroidFighter
+    {
+        public Dfs420()
+        {
+            PilotInfo = new PilotCardInfo(
+                "DFS-420",
+                4,
+                25,
+                true,
+                abilityType: typeof(Abilities.SecondEdition.Dfs420Ability),
+                tags: new List<Tags>
+                {
+                    Tags.Droid
+                },
+                extraUpgradeIcon: UpgradeType.Talent
+            );
+            ImageUrl = "https://raw.githubusercontent.com/sampson-matt/FlyCasualLegacyCustomCards/refs/heads/main/Homebrew/Dfs420.png";
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    //After you fully execute a red maneuver or perform a red barrel roll, if there is an enemy ship at Range 0-1, you may remove 1 stress token.
+    //You may perform primary attacks at Range 0.
+    public class Dfs420Ability : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.OnActionIsPerformed += CheckConditions;
+            HostShip.OnMovementFinishSuccessfully += RegisterMovementTrigger;
+            HostShip.PrimaryWeapons.ForEach(n => n.WeaponInfo.MinRange = 0);
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnActionIsPerformed -= CheckConditions;
+            HostShip.OnMovementFinishSuccessfully -= RegisterMovementTrigger;
+            HostShip.PrimaryWeapons.ForEach(n => n.WeaponInfo.MinRange = 1);
+        }
+
+        protected void CheckConditions(GenericAction action)
+        {
+            if (action.IsRed && Board.GetShipsAtRange(HostShip, new UnityEngine.Vector2(0, 1), Team.Type.Enemy).Any())
+            {
+                HostShip.OnActionDecisionSubphaseEnd += RegisterActionTrigger;
+            }
+        }
+
+        private void RegisterActionTrigger(GenericShip ship)
+        {
+            HostShip.OnActionDecisionSubphaseEnd -= RegisterActionTrigger;
+
+            RegisterAbilityTrigger(TriggerTypes.OnFreeAction, AskAbility);
+        }
+
+        protected void RegisterMovementTrigger(GenericShip ship)
+        {
+            if (HostShip.GetLastManeuverColor() == MovementComplexity.Complex && Board.GetShipsAtRange(HostShip, new UnityEngine.Vector2(0,1), Team.Type.Enemy).Any())
+            {
+                RegisterAbilityTrigger(TriggerTypes.OnMovementFinish, AskAbility);
+            }
+        }
+
+        private void AskAbility(object sender, System.EventArgs e)
+        {
+            if (HostShip.IsStressed == true)
+            {
+                AskToUseAbility(
+                    HostShip.PilotInfo.PilotName,
+                    AlwaysUseByDefault,
+                    UseAbility,
+                    descriptionLong: "Do you want to remove 1 Stress Token?",
+                    imageHolder: HostShip
+                );
+            }
+            else
+            {
+                Triggers.FinishTrigger();
+            }
+        }
+
+        private void UseAbility(object sender, System.EventArgs e)
+        {
+            HostShip.Tokens.RemoveToken(typeof(StressToken), DecisionSubPhase.ConfirmDecision);
+        }
+    }
+}

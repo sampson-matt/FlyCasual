e714ea9d7f49d3eb3c70a677cf6fe7742c649d9e
sampson-matt <sampson.matt@gmail.com>
2025-09-19 11:49:58 -0500

    Add Essara Till and Rhys Dallows

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/NabooRoyalN1Starfighter/EssaraTill.cs
@@ -0,0 +1,130 @@
+﻿using Abilities.SecondEdition;
+using ActionsList;
+using Ship;
+using SubPhases;
+using Content;
+using System.Collections.Generic;
+using Upgrade;
+using Tokens;
+using System;
+using BoardTools;
+
+namespace Ship
+{
+    namespace SecondEdition.NabooRoyalN1Starfighter
+    {
+        public class EssaraTill : NabooRoyalN1Starfighter
+        {
+            public EssaraTill() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "Essara Till",
+                    4,
+                    37,
+                    isLimited: true,
+                    abilityText: "When you or a friendly Rhys Dallows at range 0-3 would receive a green token, you may spend 1 [Charge]. If you do, that ship may perform a [Lock] action instead.",
+                    abilityType: typeof(EssaraTillAbility),
+                    extraUpgradeIcons: new List<UpgradeType>() { UpgradeType.Talent, UpgradeType.Talent }
+                );
+
+                ImageUrl = "https://raw.githubusercontent.com/sampson-matt/FlyCasualLegacyCustomCards/refs/heads/main/Homebrew/X2PO-homebrewPilot-watessaratillv23.png";
+            }
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class EssaraTillAbility : GenericAbility
+    {
+        GenericToken GreenToken;
+        public override void ActivateAbility()
+        {
+            GenericShip.BeforeTokenIsAssignedGlobal += CheckAbility;
+        }
+
+        public override void DeactivateAbility()
+        {
+            GenericShip.BeforeTokenIsAssignedGlobal -= CheckAbility;
+        }
+
+        private void CheckAbility(GenericShip ship, GenericToken token)
+        {
+            if (token.TokenColor == TokenColors.Green
+                && (IsMe(ship) || IsRhysDallowsInRange(ship)))
+            {
+                TargetShip = ship;
+                GreenToken = token;
+                RegisterAbilityTrigger(TriggerTypes.OnBeforeTokenIsAssigned, AskPerformFreeAction);
+            }
+        }
+
+        private void AskPerformFreeAction(object sender, EventArgs e)
+        {
+            if (TargetShip.Tokens.TokenToAssign != null)
+            {
+                TargetShip.BeforeActionIsPerformed += PrepareRemoveToken;
+
+                TargetShip.AskPerformFreeAction
+                (
+                    new List<GenericAction>()
+                    {
+                        new TargetLockAction()
+                    },
+                    CleanUp,
+                    HostShip.PilotInfo.PilotName,
+                    $"Essara Till: You may spend 1 charge to allow {TargetShip.PilotInfo.PilotName} to perform a Target Lock action instead of receiving a {GreenToken.Name}?",
+                    HostUpgrade
+                );
+            }
+            else
+            {
+                Triggers.FinishTrigger();
+            }
+        }
+
+        private void PrepareRemoveToken(GenericAction action, ref bool isFreeAction)
+        {
+            TargetShip.BeforeActionIsPerformed -= PrepareRemoveToken;
+
+            RegisterAbilityTrigger(TriggerTypes.BeforeActionIsPerformed, RemoveToken);
+        }
+
+        private void RemoveToken(object sender, EventArgs e)
+        {
+            TargetShip.Tokens.TokenToAssign = null;
+            Triggers.FinishTrigger();
+        }
+
+        private void CleanUp()
+        {
+            HostShip.BeforeActionIsPerformed -= PrepareRemoveToken;
+            Triggers.FinishTrigger();
+        }
+
+        private bool IsRhysDallowsInRange(GenericShip ship)
+        {
+            bool result = false;
+
+            if (ship.PilotInfo.PilotName == "Rhys Dallows" && Tools.IsFriendly(ship, HostShip))
+            {
+                DistanceInfo distanceInfo = new DistanceInfo(HostShip, ship);
+                if (distanceInfo.Range <= 3)
+                {
+                    result = true;
+                }
+            }
+
+            return result;
+        }
+
+        private bool IsMe(GenericShip ship)
+        {
+            return ship.ShipId == HostShip.ShipId;
+        }
+
+
+
+
+    }
+}
---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/NabooRoyalN1Starfighter/RhysDallows.cs
@@ -0,0 +1,130 @@
+﻿using Abilities.SecondEdition;
+using ActionsList;
+using Ship;
+using SubPhases;
+using Content;
+using System.Collections.Generic;
+using Upgrade;
+using Tokens;
+using System;
+using BoardTools;
+
+namespace Ship
+{
+    namespace SecondEdition.NabooRoyalN1Starfighter
+    {
+        public class RhysDallows : NabooRoyalN1Starfighter
+        {
+            public RhysDallows() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "Rhys Dallows",
+                    6,
+                    45,
+                    isLimited: true,
+                    abilityText: "When you or a friendly Rhys Dallows at range 0-3 would receive a green token, you may spend 1 [Charge]. If you do, that ship may perform a [Barrel Roll] action instead.",
+                    abilityType: typeof(RhysDallowsAbility),
+                    extraUpgradeIcons: new List<UpgradeType>() { UpgradeType.Talent, UpgradeType.Illicit }
+                );
+
+                ImageUrl = "https://raw.githubusercontent.com/sampson-matt/FlyCasualLegacyCustomCards/refs/heads/main/Homebrew/X2PO-homebrewPilot-watrhysdallowsv23.png";
+            }
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class RhysDallowsAbility : GenericAbility
+    {
+        GenericToken GreenToken;
+        public override void ActivateAbility()
+        {
+            GenericShip.BeforeTokenIsAssignedGlobal += CheckAbility;
+        }
+
+        public override void DeactivateAbility()
+        {
+            GenericShip.BeforeTokenIsAssignedGlobal -= CheckAbility;
+        }
+
+        private void CheckAbility(GenericShip ship, GenericToken token)
+        {
+            if (token.TokenColor == TokenColors.Green
+                && (IsMe(ship) || IsEssaraTillInRange(ship)))
+            {
+                TargetShip = ship;
+                GreenToken = token;
+                RegisterAbilityTrigger(TriggerTypes.OnBeforeTokenIsAssigned, AskPerformFreeAction);
+            }
+        }
+
+        private void AskPerformFreeAction(object sender, EventArgs e)
+        {
+            if (TargetShip.Tokens.TokenToAssign != null)
+            {
+                TargetShip.BeforeActionIsPerformed += PrepareRemoveToken;
+
+                TargetShip.AskPerformFreeAction
+                (
+                    new List<GenericAction>()
+                    {
+                        new BarrelRollAction()
+                    },
+                    CleanUp,
+                    HostShip.PilotInfo.PilotName,
+                    $"Rhys Dallows: You may spend 1 charge to allow {TargetShip.PilotInfo.PilotName} to perform a Barrel Roll action instead of receiving a {GreenToken.Name}?",
+                    HostUpgrade
+                );
+            }
+            else
+            {
+                Triggers.FinishTrigger();
+            }
+        }
+
+        private void PrepareRemoveToken(GenericAction action, ref bool isFreeAction)
+        {
+            TargetShip.BeforeActionIsPerformed -= PrepareRemoveToken;
+
+            RegisterAbilityTrigger(TriggerTypes.BeforeActionIsPerformed, RemoveToken);
+        }
+
+        private void RemoveToken(object sender, EventArgs e)
+        {
+            TargetShip.Tokens.TokenToAssign = null;
+            Triggers.FinishTrigger();
+        }
+
+        private void CleanUp()
+        {
+            HostShip.BeforeActionIsPerformed -= PrepareRemoveToken;
+            Triggers.FinishTrigger();
+        }
+
+        private bool IsEssaraTillInRange(GenericShip ship)
+        {
+            bool result = false;
+
+            if (ship.PilotInfo.PilotName == "Essara Till" && Tools.IsFriendly(ship, HostShip))
+            {
+                DistanceInfo distanceInfo = new DistanceInfo(HostShip, ship);
+                if (distanceInfo.Range <= 3)
+                {
+                    result = true;
+                }
+            }
+
+            return result;
+        }
+
+        private bool IsMe(GenericShip ship)
+        {
+            return ship.ShipId == HostShip.ShipId;
+        }
+
+
+
+
+    }
+}

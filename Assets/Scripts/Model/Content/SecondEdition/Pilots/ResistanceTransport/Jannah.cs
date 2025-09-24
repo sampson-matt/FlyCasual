6eb2342ffbf98981a4713de768f2d0f96814e7fc
sampson-matt <sampson.matt@gmail.com>
2025-09-24 14:52:02 -0500

    Add Jannah

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/ResistanceTransport/Jannah.cs
@@ -0,0 +1,73 @@
+﻿using Abilities.SecondEdition;
+using System.Collections.Generic;
+using ActionsList;
+using Ship;
+using Upgrade;
+
+namespace Ship
+{
+    namespace SecondEdition.ResistanceTransport
+    {
+        public class Jannah : ResistanceTransport
+        {
+            public Jannah() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "Jannah",
+                    5,
+                    42,
+                    isLimited: true,
+                    abilityText: "After you perform an action added to your action bar by a crew upgrade, you may perform a reinforce action.",
+                    abilityType: typeof(JannahAbility),
+                    extraUpgradeIcon: UpgradeType.Talent
+                );
+
+                ImageUrl = "https://raw.githubusercontent.com/sampson-matt/FlyCasualLegacyCustomCards/refs/heads/main/Homebrew/Jannah.png";
+            }
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class JannahAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.OnActionIsPerformed += CheckConditions;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnActionIsPerformed -= CheckConditions;
+        }
+
+        private void CheckConditions(GenericAction action)
+        {
+            if (HostShip.UpgradeBar.HasUpgradeTypeInstalled(UpgradeType.Crew) 
+                && action.Source != null 
+                && action.Source.Slot.Type == UpgradeType.Crew)
+            {
+                HostShip.OnActionDecisionSubphaseEnd += DoReinforceAction;
+            }
+        }
+
+        private void DoReinforceAction(GenericShip ship)
+        {
+            HostShip.OnActionDecisionSubphaseEnd -= DoReinforceAction;
+
+            RegisterAbilityTrigger(TriggerTypes.OnFreeAction, PerformAction);
+        }
+
+        private void PerformAction(object sender, System.EventArgs e)
+        {
+            HostShip.AskPerformFreeAction(
+                new ReinforceAction(),
+                Triggers.FinishTrigger,
+                HostShip.PilotInfo.PilotName,
+                "After you perform an action added to your action bar by a crew upgrade, you may perform a reinforce action.",
+                HostShip
+            );
+        }
+    }
+}

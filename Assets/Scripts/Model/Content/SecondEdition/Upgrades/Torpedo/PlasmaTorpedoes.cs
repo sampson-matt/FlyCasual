30d761bccdf75e8c10e02ff0a1f9b91b56a77b5d
sampson-matt <sampson.matt@gmail.com>
2026-01-22 09:53:32 -0600

    update torpedo urls

--- Assets/Scripts/Model/Content/SecondEdition/Upgrades/Torpedo/PlasmaTorpedoes.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Torpedo/PlasmaTorpedoes.cs
@@ -23,8 +23,6 @@ namespace UpgradesList.SecondEdition
                 ),
                 abilityType: typeof(Abilities.SecondEdition.PlasmaTorpedoesAbility)
             );
-
-            ImageUrl = "https://images-cdn.fantasyflightgames.com/filer_public/6f/83/6f83abcd-9460-4208-a439-f6a81597f5f0/swz40_card-plasma-torpedoes.png";
         }
 
         public void UpdateCost(GenericShip ship)
